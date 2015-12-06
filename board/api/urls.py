b3925b55be91e25befb84cffaa2bf6336c1f49b9
Renan Palmeira <renan@forleven.com>
2015-12-05 23:39:16 -0200

    New Viewset's in urls

--- board/api/urls.py
+++ board/api/urls.py
@@ -3,7 +3,9 @@ from __future__ import unicode_literals
 from django.conf.urls import url, include
 from rest_framework.routers import DefaultRouter
 
+from board.app.project.views import ProjectViewSet
 from board.app.social.views import SocialViewSet
+from board.core.application.views import ApplicationViewSet
 from board.core.account.views import AccountViewSet
 from board.core.configuration.views import ConfigurationViewSet
 
@@ -11,3 +13,5 @@ router = DefaultRouter()
 router.register(r'configuration', ConfigurationViewSet, 'Gear')
 router.register(r'account', AccountViewSet, 'Account')
 router.register(r'social', SocialViewSet, 'Social')
+router.register(r'project', ProjectViewSet, 'Project')
+router.register(r'application', ApplicationViewSet, 'Application')
--- board/api/views/viewset.py
+++ board/api/views/viewset.py
@@ -6,18 +6,12 @@ from board.api.pagination import CustomPagination
 class ViewSet(viewsets.ModelViewSet):
 	def get_queryset(self):
 		queryset = self.model.objects.all()
-		loadtype = self.request.query_params.get('loadtype', None)
-		if loadtype == 'results':
-		    self.pagination_class = CustomPagination
-
 		query = self.request.query_params.get('q', None)
-
 		if query:
 			try:
 				queryset = [self.model.objects.get(slug=query)]
 			except Exception, e:
 				pass
-
 		return queryset
 
 	@decorators.detail_route(methods=['get',])
