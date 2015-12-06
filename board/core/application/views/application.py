75e6b091a595cbfaec341ac74841d1f377235b30
Renan Palmeira <renan@forleven.com>
2015-12-05 23:40:34 -0200

    New api method application

--- board/core/application/views/application.py
+++ board/core/application/views/application.py
@@ -1,22 +1,9 @@
 from __future__ import unicode_literals
 
-from django.shortcuts import render, get_object_or_404
-from django.utils.module_loading import import_string
-from django.contrib.auth.decorators import login_required
+from board.api.views import ViewSet
+from board.core.application.serializers import ApplicationSerializer, Application
 
-from board.core.application.models import Application
-
-from django.template import loader, TemplateDoesNotExist
-
-@login_required
-def home(request, slug = None):
-	context = {}
-	if not slug is None:
-		app = get_object_or_404(Application, slug=slug)
-		try:
-			view = import_string('board.app.%s.views.home' % slug)
-			return view(request)
-		except Exception, e:
-			print e
-		
-	return render(request, "application/application.html", context)
+class ApplicationViewSet(ViewSet):
+	model = Application
+	serializer_class = ApplicationSerializer
+	lookup_field = 'slug'
