f3bd0653cf59d6d5e16799cac97d2ce145a218db
Renan Palmeira <renan@forleven.com>
2015-12-19 23:26:31 -0200

    Added validation and save of ArticleForm lack a response friendly to client

--- board/app/news/views/news.py
+++ board/app/news/views/news.py
@@ -2,14 +2,34 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+
+from django.utils.translation import ugettext as _
 
 from board.app.news.models import ArticleConfiguration
+from board.app.project.models import Project
 from board.app.news.forms import ArticleForm
 
 @login_required
 def home(request):
 	form = ArticleForm()
 	configuration = ArticleConfiguration.objects.all()
+
+	if request.method == 'POST':
+		project = Project.objects.last()
+		form = ArticleForm(request.POST)
+		if form.is_valid():
+			try:
+				article = form.save(commit=False)
+				article.account = request.user
+				article.project = project
+				article.save()
+				messages.success(request, _('News saved'))
+			except Exception, e:
+				messages.error(request, _('Erro interno 500 - ')+str(e))
+		else:
+			messages.error(request, _('incorrect fields'))
+
 	context = {
 		'form': form,
 		'configuration': configuration,
