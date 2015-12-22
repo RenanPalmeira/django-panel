4122cf363d238f8b45a8fc797fe1d7fee424eb32
Renan <renan@forleven.com>
2015-12-22 17:30:24 -0200

    Correction in variables TWITTER of settings

--- board/core/provider/twitter.py
+++ board/core/provider/twitter.py
@@ -1,9 +1,9 @@
 from __future__ import unicode_literals
 
 from django.conf import settings
-from tweepy import OAuthHandler
+from tweepy import OAuthHandler, API
 
-TWITTER_API_KET =  settings.TWITTER_API_KET
+TWITTER_API_KEY =  settings.TWITTER_API_KEY
 TWITTER_API_SECRET = settings.TWITTER_API_SECRET
 
-TwitterProvider = OAuthHandler(TWITTER_API_KET, TWITTER_API_SECRET)
+TwitterProvider = OAuthHandler(TWITTER_API_KEY, TWITTER_API_SECRET)
