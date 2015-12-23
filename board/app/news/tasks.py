e486477f96dcdee53502989c78979ededb05e184
Renan Palmeira <renan@forleven.com>
2015-12-23 14:24:53 -0200

    New task in news.tasks for crawler a link in reference for Articles

--- board/app/news/tasks.py
+++ board/app/news/tasks.py
@@ -1,10 +1,21 @@
 from __future__ import unicode_literals, absolute_import
 
+import re
 import logging
+import requests
 from celery.task import task
 
 logger = logging.getLogger(__name__)
 
+ep = r'^http?s\:\/\/([a-z0-9\.\-\/\%\&])$'
+
 @task
-def send_link(x, y):
-    return x + y
+def send_link(reference):
+	try:
+		result = dict({})
+		link = requests.get(reference)
+		result['title'] = re.findall(r'<title>([a-z0-9A-Z\.\\\!\? ]+)</title>', link.text)
+		return result
+	except Exception, e:
+		logger.warning(e)
+	return None
