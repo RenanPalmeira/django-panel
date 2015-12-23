3d2be267eb3bd475b279bfedd525098ad8d7cc11
Renan Palmeira <renan@forleven.com>
2015-12-23 14:27:52 -0200

    Tests in travis with celery and redis

--- board/app/news/tests/test_model.py
+++ board/app/news/tests/test_model.py
@@ -6,6 +6,8 @@ from board.core.account.models import Account
 from board.app.news.models import Article
 from board.app.project.models import Project
 
+from board.app.news.tasks import send_link
+
 class ArticleTestCase(TestCase):
 	fixtures = ['account.json', ]
 	def setUp(self):
@@ -17,7 +19,11 @@ class ArticleTestCase(TestCase):
 	def test_model(self):
 		self.article.account = self.account
 		self.article.project = self.project
+		self.article.reference = 'https://www.python.org'
 
 		self.article.title = 'Hello world'
 		self.article.content = '...'
 		self.article.save()
+
+	def test_tasks(self):
+		result= send_link.delay('https://www.python.org')
